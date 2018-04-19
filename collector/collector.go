051ccb9c97a12d4d020c8bc1e59eaa99a8ce5728
tzulin.chin <tzulin.chin@rakuten.com>
2018-04-19 14:40:06 +0900

    Fix dirSize bug

--- collector/collector.go
+++ collector/collector.go
@@ -126,10 +126,13 @@ func FetchDiskUsage(user User, parameters map[string]string, ch chan<- float64) 
 func dirSize(path string) (int64, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+                if err != nil {
+                  return err
+                }
 		if !info.IsDir() {
 			size += info.Size()
 		}
-		return err
+		return nil
 	})
 	return size, err
 }
